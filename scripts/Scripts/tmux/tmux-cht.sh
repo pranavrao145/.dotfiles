756a2fba30bc6addc6d7334be8e52509d1649f09
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-21 15:25:09 -0400

    fixed i3 config

--- scripts/Scripts/tmux/tmux-cht.sh
+++ scripts/Scripts/tmux/tmux-cht.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 selected=`cat ~/Scripts/tmux/.tmux-cht-languages ~/Scripts/tmux/.tmux-cht-commands | fzf`
-
-read -p "Enter query: " query
-
-if grep -qs "$selected" ~/Scripts/tmux/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done" && tmux a
-else
-    tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done" && tmux a
+if [[ -z $selected ]]; then
+    exit 0
 fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done" && tmux a
+else
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less" && tmux a
+fi
+
